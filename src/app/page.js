4258ae818eb00b1374133f27aa6c922a1847eaab
bellasch19 <68909697+bellasch19@users.noreply.github.com>
2023-10-26 11:09:20 +0200

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import Navbar from './components/navbar'
+/**
+ * 
+ * @returns home screen functionality bella
+ */
 export default function Home() {
   return (
-   <p> hello </p>
+    <p> hello </p>
   )
 }

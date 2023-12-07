ab8714dcf4a80a92d4c21c46e46b11560aa98011
bellasch19 <68909697+bellasch19@users.noreply.github.com>
2023-12-07 20:42:37 +0200

    Home page slider tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -104,7 +104,7 @@ export default function Home() {
           <div id="default-carousel" className="relative w-full" data-carousel="slide">
             <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="./images/joe-img.jpeg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                <img src="./images/Screenshot 2023-12-01 at 18.29.23.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
               </div>
               <div className="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="./images/bella-img.jpeg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />

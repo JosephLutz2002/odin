bcb93f06d0c73d7cd9349c9242c67c3730b36e80
bellasch19 <68909697+bellasch19@users.noreply.github.com>
2024-02-24 10:45:08 +0200

    Slider component

--- src/app/components/slider.js
+++ src/app/components/slider.js
@@ -1,4 +1,26 @@
+'use client'
 import React from "react";
+import Link from "next/link";
 import "./global.css"
 
-const 
+const Slider = () => {
+    const filteredItems = [
+        {
+            id: 1,
+            img: "",
+        },
+        {
+            id: 2,
+            img: "",
+        },
+        {
+            id: 3,
+            img: "",
+        },
+        {
+            id: 4,
+            img: "",
+        },
+    ];
+
+}

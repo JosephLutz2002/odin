3409be81c5b17de728f432e3d45b8ea73675674f
bellasch19 <68909697+bellasch19@users.noreply.github.com>
2024-03-11 11:40:52 +0200

    Slider base complete and added to homepage

--- src/app/components/slider.js
+++ src/app/components/slider.js
@@ -1,26 +1,57 @@
 'use client'
 import React from "react";
 import Link from "next/link";
-import "./global.css"
+import '.src/app/globals.css'
 
 const Slider = () => {
     const filteredItems = [
         {
             id: 1,
+            description: "The Overview",
             img: "",
         },
         {
             id: 2,
-            img: "",
-        },
-        {
-            id: 3,
-            img: "",
-        },
-        {
-            id: 4,
+            dsescription: "The Dashboard",
             img: "",
         },
     ];
+    const slideLeft = () => {
+        let slider = document.getElementById("slider");
+        slidedr.scrollLeft = slider.scrollLeft - 235;
+    }
 
-}
+    const slideRight = () => {
+        let slider = document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft + 235;
+    }
+
+    return (
+        <>
+            <div className="slider-container">
+                <div className="btn">
+                    <button title="scroll left" onClick={slideLeft}>
+                        <AiOutlineArrowLeft />
+                    </button>
+                    <button title="scroll right" onClick={slideRight}>
+                        <AiOutlineArrowRight />
+                    </button>
+                </div>
+                <div className="row-container" id="image-slider">
+                    {filteredItems.map((item) => (
+                        <div key={item.id} className="row-item">
+                            <Link href={`/`}>
+                                <div>
+                                    <img src={item.img} alt="odin interface screenshot" />
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+export default Slider;
+
+
